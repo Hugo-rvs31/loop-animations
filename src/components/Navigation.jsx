fcbf2f954f6b14d3839964e765d74819fbd81fca
Hugo-rvs31 <hug.rvs31@gmail.com>
2026-02-05 18:58:23 +0100

    first commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
+
+const Navigation = () => {
+  return <NavLink></NavLink>;
+};
+
+export default Navigation;
